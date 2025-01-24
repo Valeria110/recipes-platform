7b1a4dca58395ad04a3ef04b79848b17cadf2549
Valeria Ivashkevich <117567259+Valeria110@users.noreply.github.com>
2025-01-24 14:32:47 +0300

    feat/recipe details page (#4)
    * feat: config docker
    
    * chore: change docker config
    
    * fix: change env file path in docker config

--- server/src/recipe/types/recipe-filters.interface.ts
+++ server/src/recipe/types/recipe-filters.interface.ts
@@ -3,11 +3,9 @@ import { Prisma } from '@prisma/client';
 export interface IRecipeFilters {
   category?: RecipeFilter;
   cuisineType?: RecipeFilter;
-  // category?: string;
-  // cuisineType?: string;
 }
 
 type RecipeFilter = {
   in: string[];
-  mode?: Prisma.QueryMode;
+  mode?: 'insensitive';
 };
