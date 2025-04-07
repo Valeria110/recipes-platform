d16d390c1e56296ddf86d0d95c30c6891e703e03
Valerie Ivashkevich <ivashkevich03@gmail.com>
2025-04-07 17:44:35 +0300

    refactor: update styles

--- client/src/widgets/layout/ui/client/BurgerMenu/BurgerMenu.tsx
+++ client/src/widgets/layout/ui/client/BurgerMenu/BurgerMenu.tsx
@@ -31,7 +31,7 @@ export const BurgerMenu = () => {
 
   return (
     <>
-      <header className='md:hidden sticky flex justify-between items-center top-0 left-0 w-dvw h-28 p-8 bg-orange-400 z-20'>
+      <header className='md:hidden sticky flex justify-between items-center top-0 left-0 w-dvw h-24 p-8 bg-orange-400 z-20'>
         <Logo onClick={closeMenu} />
         <button onClick={toggleBurgerBtn} className='flex flex-col gap-1 justify-center items-center w-11 h-11 ml-auto'>
           <span
@@ -43,7 +43,7 @@ export const BurgerMenu = () => {
         </button>
       </header>
       <div
-        className={`fixed top-28 w-dvw h-fit flex flex-col gap-10 justify-center items-center md:hidden bg-orange-400 p-5 z-20 ${styles.menuContainer} ${isMenuOpen && styles.openMenu} `}
+        className={`fixed top-24 w-dvw h-full flex flex-col gap-10 justify-center items-center md:hidden bg-orange-400 p-5 z-20 ${styles.menuContainer} ${isMenuOpen && styles.openMenu} `}
       >
         <NavBar onClick={closeMenu} />
         <ProfileToolKit closeMenu={closeMenu} />
